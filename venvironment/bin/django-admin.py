d5c0fa3a5b775f9ea71cf0e2a5486182e5eaf28e
chirag <chirag@healthcloudai.com>
2020-07-16 20:46:21 +0530

    Initial commit

---
+++ venvironment/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/chirag/software/TestWebsite/udaan_hr_app/venvironment/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
